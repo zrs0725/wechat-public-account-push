b0f69c0ff8e6872f33967ed23aae6b02cd54f7d8
evanglong <61139802+evanglong@users.noreply.github.com>
2022-09-06 09:17:28 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -31,26 +31,27 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "欧阳憨憨",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
+      id: "o0plv51RxAk1h4W0q8Ww4PuvtWaw",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
+      useTemplateId: "kU3PiJQxRjY9l6uKOqFxdWQkFViUwdu-NfrPFZynJKE",
       // 所在省份
       province: "广东",
       // 所在城市
-      city: "肇庆",
+      city: "广州",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn"
     }, 
     {
-      name: "老婆1", 
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      openUrl: "https://wangxinleo.cn"
+      name: "本机", 
+      id: "o0plv54YCWE5xO6zWUwkqE2wCE2c",
+      useTemplateId: "kU3PiJQxRjY9l6uKOqFxdWQkFViUwdu-NfrPFZynJKE",
+      province: "广东",
+      city: "珠海",
+      openUrl: "https://baidu.com"
     }, 
+    /*
     {
       name: "老婆2",
       id: "",
@@ -68,13 +69,13 @@ export const config = {
       openUrl: "https://wangxinleo.cn"
     }, 
   ],
-
+*/
   /**
    * 回调消息 相关，主要用来展示发送是否成功/失败的数据
    */
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "zjV1glIuCXo4Csr23aXWEio1iP74sZJ7IQEES95lDsg6c4yOU",
+  CALLBACK_TEMPLATE_ID: "FucC7UQKf91STVxuEJZLmB5RCrlrm7Hj_L8O37ibWAk",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -82,7 +83,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohx12OO6VskYv03Kuf43ywIZVRM4puXhk",
+      id: "o0plv54YCWE5xO6zWUwkqE2wCE2c",
     }, 
   ],
     
@@ -95,7 +96,7 @@ export const config = {
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
   PROVINCE: "广东",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "广州",
 
   /** 重要节日相关 */
 
@@ -105,11 +106,11 @@ export const config = {
    * 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
    * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
    */
-  FESTIVALS: [
-    {"type": "生日", "name": "老婆", "year": "1996", "date": "09-02"},
-    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
-    {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-    {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+   FESTIVALS: [
+    {"type": "生日", "name": "欧阳", "year": "2000", "date": "01-13"},
+    {"type": "节日", "name": "元旦纪念日", "year": "2021", "date": "01-01"},
+    //{"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
+    //{"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
   ],
 
   /**
@@ -117,7 +118,7 @@ export const config = {
    * 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
    * 如果为0, 则默认展示全部
    */
-  FESTIVALS_LIMIT: 4,
+  FESTIVALS_LIMIT: 0,
 
   /** 日期相关 */
 
@@ -127,11 +128,11 @@ export const config = {
     * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    {"keyword": "love_day", date: "2015-05-01"},
+    {"keyword": "love_day", date: "2021-01-01"},
     // 结婚纪念日
-    {"keyword": "marry_day", date: "2020-01-04"},
+    //{"keyword": "marry_day", date: "2020-01-04"},
     // 退伍日, 不用可以删掉
-    {"keyword": "ex_day", date: "2022-08-31"},
+    //{"keyword": "ex_day", date: "2022-08-31"},
     // sakana日
     // {"keyword": "sakana_day", date: "2022-01-06"},
     // ... 
